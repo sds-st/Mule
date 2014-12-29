31f06c0f943eee0d1985883fe96ae3790923b52e
edaum <edaum@cisco.com>
2014-12-29 08:52:30 +0000

    Create installAnypointStudio.sh

---
+++ install/installAnypointStudio.sh
@@ -0,0 +1,10 @@
+# ============================================================================
+# Anypoint Studio
+# http://www.mulesoft.com/ty/dl/studio-linux
+
+wget 'https://mule-studio.s3.amazonaws.com/4.1.0-OCT14/AnypointStudio-for-linux-64bit-4.1.0-201410161901.tar.gz'
+tar -zxvf AnypointStudio-for-linux-64bit-4.1.0-201410161901.tar.gz
+rm AnypointStudio-for-linux-64bit-4.1.0-201410161901.tar.gz
+chmod u+x AnypointStudio/AnypointStudio
+./AnypointStudio/AnypointStudio
+
