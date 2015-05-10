51c5dd247e354d356ee006cf3540d99970bf556a
edaum <edaum@cisco.com>
2015-05-10 10:22:45 +0300

    Update installMuleStandalone.sh

--- install/installMuleStandalone.sh
+++ install/installMuleStandalone.sh
@@ -1,33 +1,40 @@
 # Mule standalone
 # https://code.google.com/p/soi-toolkit/wiki/IG_RT_MuleESB
 
+#muleVersion="3.4.0"
+#muleVersion="3.3.0"
+muleVersion="3.6.1"
 
-wget 'http://dist.codehaus.org/mule/distributions/mule-standalone-3.3.0.tar.gz'
-sudo tar -xvf mule-standalone-3.3.0.tar.gz -C /usr/local
-rm mule-standalone-3.3.0.tar.gz
-sudo ln -s /usr/local/mule-standalone-3.3.0/bin/mule /etc/init.d/mule_ce_3.3.0
-sudo update-rc.d mule_ce_3.3.0 start 67 2 3 4 5 . stop 67 0 1 6 .
-sudo service mule_ce_3.3.0 start
+wget 'https://repository-master.mulesoft.org/nexus/content/repositories/releases/org/mule/distributions/mule-standalone/'${muleVersion}'/mule-standalone-'${muleVersion}'3.6.1.tar.gz'
+#wget 'http://dist.codehaus.org/mule/distributions/mule-standalone-'${muleVersion}'.tar.gz'
+sudo tar -xvf mule-standalone-${muleVersion}.tar.gz -C /usr/local
+rm mule-standalone-${muleVersion}.tar.gz
+
+#create service
+sudo ln -s /usr/local/mule-standalone-${muleVersion}/bin/mule /etc/init.d/mule_s
+sudo update-rc.d mule_s start 67 2 3 4 5 . stop 67 0 1 6 .
+sudo service mule_s start
 
 #Useful commands
 #
 # sudo reboot
 
 # to view log
-# tail /usr/local/mule-standalone-3.3.0/logs/mule.log -n50
+# tail /usr/local/mule-standalone-${muleVersion}/logs/mule.log -n50
 
 
 #To verify if Mule is running use the command:
-#$ sudo service mule_ce_3.3.0 status
+#$ sudo service mule_s status
 
 #To restart Mule use the command:
-#$ sudo service mule_ce_3.3.0 restart
+#$ sudo service mule_s restart
 
 #To stop Mule use the command:
-#$ sudo service mule_ce_3.3.0 stop
+#$ sudo service mule_s stop
 
 #To uninstall the Mule service, do the following:
-#$ sudo update-rc.d -f mule_ce_3.3.0 remove
-#$ sudo rm /etc/init.d/mule_ce_3.3.0
+#$ sudo update-rc.d -f mule_s remove
+#$ sudo rm /etc/init.d/mule_s
+
 
 
